2a2dad3380845022beb6586a88850ae3759fa220
Aäron Declerck <del-aire@users.noreply.github.com>
2017-05-23 20:34:01 +0100

    Renaming

--- Mock/service.Server.js
+++ Mock/service.Server.js
@@ -18,10 +18,9 @@ setInterval(function () {
         dhtHumidity: 30,
         dhtTemperature: 21,
 
-        gpsTime: `${aDate.getHours()}:${('0' + aDate.getMinutes()).substr(-2)}:${('0' + aDate.getSeconds()).substr(-2)}`,
-        gpsDate: '16-05-2017',
-        gpsLatitude: 48.8583,
-        gpsLongitude: 2.2944,
+        gpggaTime: `${aDate.getHours()}:${('0' + aDate.getMinutes()).substr(-2)}:${('0' + aDate.getSeconds()).substr(-2)}`,
+        gpggaLatitude: 48.8583,
+        gpggaLongitude: 2.2944,
 
         dn7c3ca007ParticleMatter2Dot5: (Math.random() * 5).toFixed(4)
     })
